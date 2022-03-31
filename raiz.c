66528c8594c8c9c7265fe923efaa9da3b04c3b02
Adriel <adriel.fariasan@gmail.com>
2022-03-30 21:41:45 -0300

    Adiciona script de calculo de raiz quadrada

---
+++ raiz.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// ADRIEL FARIA DOS SANTOS - 30/03/2022
+// UFRN - CERES - CAMPUS CAICÓ
+
+int main(int argc, char** argv) {
+	printf("%lf\n", sqrt(atof(argv[1])));
+	return 0;
+}
